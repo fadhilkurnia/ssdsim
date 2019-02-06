d981eeb45335a23021e41a43e647d24435dbcc8b
Fadhil Imam Kurnia <fadhilimamk@gmail.com>
2019-02-07 02:05:14 +0700

    add tracefile in raid struct

--- raid.c
+++ raid.c
@@ -3,13 +3,16 @@
 #include "raid.h"
 #include "ssd.h"
 
+// initialize_raid function initializes raid struct and all ssd connected to the struct
 struct raid_info* initialize_raid(struct raid_info* raid, struct user_args* uargs) {
     struct ssd_info *ssd_pointer;
 
-    raid->num_disk = NDISK;
+    raid->num_disk = uargs->num_disk;
+    strcpy(raid->tracefilename, uargs->trace_filename);
+
     raid->connected_ssd=malloc(sizeof(struct ssd_info) * raid->num_disk);
     alloc_assert(raid->connected_ssd, "connected ssd");
-    for (int i = 0; i < NDISK; i++) {
+    for (int i = 0; i < raid->num_disk; i++) {
         ssd_pointer=(struct ssd_info*) malloc(sizeof(struct ssd_info));
         alloc_assert(ssd_pointer, "one of connected ssd");
         memset(ssd_pointer,0,sizeof(struct ssd_info));
@@ -18,15 +21,13 @@ struct raid_info* initialize_raid(struct raid_info* raid, struct user_args* uarg
 
 
     // initialize each ssd in raid
-    for (int i = 0; i < NDISK; i++) {
+    for (int i = 0; i < raid->num_disk; i++) {
         printf("Initializing disk%d\n", i);
 
         raid->connected_ssd[i] = initialize_ssd(raid->connected_ssd[i], uargs);
         raid->connected_ssd[i] = initiation(raid->connected_ssd[i]);
         raid->connected_ssd[i] = make_aged(raid->connected_ssd[i]);
         raid->connected_ssd[i] = pre_process_page(raid->connected_ssd[i]);
-
-        display_freepage(raid->connected_ssd[i]);
     }
 
     return raid;
@@ -52,18 +53,29 @@ int simulate_raid(struct user_args* uargs) {
         exit(1);
     }
     
+    free_raid_ssd(raid);
     free(raid);
     return 0;
+}
+
+void free_raid_ssd(struct raid_info* raid) {
+    for (int i = 0; i < raid->num_disk; i++) {
+        free(raid->connected_ssd[i]);
+    }
 }
 
 struct raid_info* simulate_raid0(struct raid_info* raid) {
     printf("Work in progress for simualting raid 0 ...\n");
 
+    raid->tracefile = fopen(raid->tracefilename, "r");
+
+    fclose(raid->tracefile);
     return raid;
 }
 
 
 struct raid_info* simulate_raid5(struct raid_info* raid) {
+    printf("Work in progress for simualting raid 5 ...\n");
 
     // controller's request queue
     // get_raid_request(raid)
--- raid.h
+++ raid.h
@@ -6,6 +6,7 @@
 #define NDISK 4
 
 struct raid_info* initialize_raid(struct raid_info*, struct user_args*);
+void free_raid_ssd(struct raid_info*);
 
 int simulate_raid(struct user_args *);
 struct raid_info* simulate_raid0(struct raid_info*);
@@ -14,6 +15,10 @@ struct raid_info* simulate_raid5(struct raid_info*);
 struct raid_info {
     unsigned int raid_type;
     unsigned int num_disk;
+
+    char tracefilename[80];
+    FILE * tracefile;
+
     struct request *request_queue;
     struct request *request_tail;
 
